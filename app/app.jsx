de4698b3387989f8e34a1f768bbf1994050904ef
Dave Harned <dharned@gmail.com>
2017-01-02 13:04:04 -0600

    first commit

--- app/app.jsx
+++ app/app.jsx
@@ -1,7 +1,47 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+const React = require('react');
+const ReactDOM = require('react-dom');
+const Axios = require('axios');
+
+const DaySchedule = React.createClass({
+	getInitialState: function() {
+		return {
+			schedule: []
+		};
+	},
+	componentDidMount: function() {
+		var th = this;
+		this.serverRequest = Axios.get('../data.json')
+			.then(function(result) {
+				th.setState({
+					schedule: result.data.schedule
+				});
+			})
+	},
+    render: function() {
+        return (
+            <tbody>
+				<tr>
+					<td>8</td>
+					<td>Appointment</td>
+				</tr>
+				<tr>
+					<td>9</td>
+					<td>Appointment</td>
+				</tr>
+				<tr>
+					<td>10</td>
+					<td>Appointment</td>
+				</tr>
+				<tr>
+					<td>11</td>
+					<td classList="booked">Appointment</td>
+				</tr>
+			</tbody>
+        )
+    }
+});
 
 ReactDOM.render(
-		<h1> BoilerPlate App</h1>,
+		<DaySchedule />,
 		document.getElementById('app')
 		);
